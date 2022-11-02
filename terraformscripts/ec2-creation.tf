8b8c7797fefd60189ec10b7f87c260f9a2d2551c
vishnukrn1996 <98821332+vishnukrn1996@users.noreply.github.com>
2022-11-02 16:53:40 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "AWSEC2Instance"{
      count   = 1
      ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
-     security_groups = ["launch-wizard-2"]
+     security_groups = ["launch-wizard-43"]
      key_name        = "recentkeypair"
      tags = {
         Name = "hostserver1"
