9a27c887b2edfd7d036b48f6d3c45fba29dec059
vishnukrn1996 <98821332+vishnukrn1996@users.noreply.github.com>
2022-11-02 16:10:30 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,12 +5,12 @@ provider "aws"{
 
 resource "aws_instance" "AWSEC2Instance"{
      count   = 1
-     ami     =  "ami-051f0947e420652a9"
+     ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
-     key_name        = "devopsmss"
+     key_name        = "recentkeypair"
      tags = {
-        Name = "tomcatserver"
+        Name = "hostserver1"
      }
 }
 
